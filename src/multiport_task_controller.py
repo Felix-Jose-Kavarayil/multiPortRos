fdc938edb432dc4de425199ffa05fac319dd53ee
Felix-Jose-Kavarayil <felixjosekavarayil@gmail.com>
2022-05-17 13:30:56 +0200

    new task, light switch off after reward

--- src/multiport_task_controller.py
+++ src/multiport_task_controller.py
@@ -29,12 +29,24 @@ def callbackIRBeam(data):
     Deliver water if the beam is broken while the light is on
     """
     global lastRewardTime
+    global nextLightChange
+    global lightStatus
     
     now = time.time()
+    #print("{} {} {}".format(data.frame_id,lightStatus,isProbeTrial))
     if data.frame_id == "broken" and lightStatus == 1 and lastRewardTime+refractoryDurationSec < now and isProbeTrial == False:
+        
         pubCommand.publish(2) # give reward
         lastRewardTime=time.time()
+        sleep(0.1)
+        
+        # switch off the light after a reward
+        pubCommand.publish(0)
+        lightStatus=0
+        nextLightChange = time.time()+lightOffDurationSec+np.random.randint(low=-5, high=5, size=1)[0]
 
+
+        
 
 defaultDrive="/ext_drives/d62/data/electro/" ## some drives have /data/electro and other /data/processing
 defaultDatabase="/adata/electro/"
@@ -47,9 +59,11 @@ localData=os.getenv("HOME") + '/'
 parser = argparse.ArgumentParser()
 parser.add_argument("mouse",help="set the mouse name")
 parser.add_argument("-s", "--sessionDuration",help="set the session duration in seconds",type=int, action="store",default = "10")
-parser.add_argument("-l", "--lightDuration",help="set the light duration in seconds",type=int, action="store",default = "1")
+parser.add_argument("-l", "--lightOnDuration",help="set the light on duration in seconds",type=int, action="store",default = "20")
+parser.add_argument("-o", "--lightOffDuration",help="set the light off duration in seconds",type=int, action="store",default = "40")
+
 parser.add_argument("-r", "--refractoryDuration",help="set the refractory period in seconds between two rewards",type=float, action="store",default = "2")
-parser.add_argument("-p", "--probeTrialProportion",help="set the proportion of probe trials (from 0 to 1)",type=float, action="store",default = ".10")
+parser.add_argument("-p", "--probeTrialProportion",help="set the proportion of probe trials (from 0 to 1)",type=float, action="store",default = "0.0")
 parser.add_argument("-d","--directory", help="create the direcotry for the data in "+defaultDatabase,action="store_true")
 parser.add_argument("-t","--transfer", help="transfer the data files to the data directory in " + defaultDatabase,action="store_true")
 
@@ -60,10 +74,12 @@ args = parser.parse_args()
 task=ntpath.basename(sys.argv[0])
 mouseName=args.mouse
 sessionDurationSec=args.sessionDuration
-lightDurationSec = args.lightDuration
+lightOnDurationSec = args.lightOnDuration
+lightOffDurationSec = args.lightOffDuration
 refractoryDurationSec = args.refractoryDuration
 probeTrialProportion = args.probeTrialProportion
 isProbeTrial = False
+
 
 
 now = datetime.datetime.now()
@@ -73,13 +89,13 @@ rospy.set_param("file_base", fileBase) # used by logger
 
 
 
-print("sessionName:",sessionName,"Session duration:",sessionDurationSec,"Light duration:",lightDurationSec,"Refractory on reward:",refractoryDurationSec, "Probe trial proportion:",probeTrialProportion)
+print("sessionName:",sessionName,"Session duration:",sessionDurationSec,"Light on duration:",lightOnDurationSec,"Light off duration:", lightOffDurationSec ,"Refractory on reward:",refractoryDurationSec, "Probe trial proportion:",probeTrialProportion)
 print("file:",fileBase)
 
 rospy.init_node('multiport_task')
 
 
-pubCommand = rospy.Publisher('multi_port_control',Int32,queue_size=1)
+pubCommand = rospy.Publisher('multi_port_control',Int32,queue_size=2)
 pubTaskEvent = rospy.Publisher('task_event',Header,queue_size=1)
 rospy.Subscriber("multi_port_ir_report", Header, callbackIRBeam)
 
@@ -100,7 +116,7 @@ timeout = time.time() + sessionDurationSec ## time at which to stop
 pubCommand.publish(0)
 lightStatus=0
 lastLightChange = time.time()
-nextLightChange = lastLightChange+lightDurationSec
+nextLightChange = lastLightChange+lightOffDurationSec + np.random.randint(low=-5, high=5, size=1)[0]
 
 
 while True:
@@ -112,20 +128,21 @@ while True:
             
             # decide if this is a light trial, get a random number from 0 to 1, compare to our proportion of probe trials
             myRand = np.random.uniform()
+            #print("rand:",myRand, "probe prop.:",probeTrialProportion)
             if myRand < probeTrialProportion:
                 isProbeTrial=True
                 msg.frame_id="probe"
-                msg.stamp=rospy.get_rostime()+ rospy.Duration(lightDurationSec/2)  # add time so it is in the middle of the light interval
+                msg.stamp=rospy.get_rostime()+ rospy.Duration(lightOnDurationSec/2)  # add time so it is in the middle of the light interval
                 pubTaskEvent.publish(msg)
             else:
                 isProbeTrial=False
             lightStatus=1
+            nextLightChange = time.time()+lightOnDurationSec
         else: # light will turn off
             pubCommand.publish(0)
             lightStatus=0
-        nextLightChange = time.time()+lightDurationSec
+            nextLightChange = time.time()+lightOffDurationSec+np.random.randint(low=-5, high=5, size=1)[0]
     
-
     sleep(0.1)
     # stop the loop when session is done
     if time.time() > timeout :
